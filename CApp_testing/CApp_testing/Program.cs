8e619bc6f71c4f60001c5f5f86d855f4ddaae7cd
khaliboy <96844280+khaliboy@users.noreply.github.com>
2022-01-07 20:34:38 +0500

    Add files via upload

---
+++ CApp_testing/CApp_testing/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CApp_testing
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Enter any number:)");
+            int number = int.Parse(Console.ReadLine());
+            bool prime = true;
+
+            for (int i = 2; i < number/2; i++)
+            {
+                if(number % i ==0)
+                {
+                    prime = false;
+                    break;
+                }
+            }
+            if(prime)
+            {
+                Console.WriteLine("number is prime.");
+            }
+            else
+            {
+                Console.WriteLine("number is not prime.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
